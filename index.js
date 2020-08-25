d5486b395f9ce9f91b52d00fe52fd080a90f59d3
Zac Yauney <zac.yauney@gmail.com>
2020-08-25 14:19:40 -0600

    Multiple form inputs with data types set

--- index.js
+++ index.js
@@ -14,22 +14,55 @@ app.use(fileUpload())
 
 const port = 8000
 
+// PUG template setup
+
+const pug = require('pug')
+const mainPage = pug.compileFile('page.pug')
+
+// These are the fields that we are gathering from the user
+
+const fields = {
+	"Mewtwo": "text",
+	"Celebi": "file",
+	"Lugia": "file",
+	"Ho-oH": "text"
+}
+
+// Create lists of names and field types
+
+const keys = Object.keys(fields)
+
+
+//Serve the main page
+
 app.get('/', async (req, res) => {
-  res.sendFile(path.join(__dirname + '/page.html'))
+	console.log(Object.keys(fields))
+	res.send(mainPage({'keys': keys, 'types':fields}))
 })
 
 app.post('/', (req, res) => {
-  var filename = 'test.py'
-  var data = req.files.inputFile.data
-  var fd = fs.openSync(filename, 'w')
-  fs.writeSync(fd, data)
-  fs.closeSync(fd)
-  var process = spawn('/usr/bin/python3', [filename])
-  var result = process.stdout
-  console.log(result)
-  res.end(result)
-  // res.send('test')
+	ans = ""
+	//fields.forEach(element => {
+		//ans += element + ": " + req.body[element] + "\n"
+	//})
+
+	res.send(ans)
 })
+
+// Handle the posted result
+
+//app.post('/', (req, res) => {
+  //var filename = 'test.py'
+  //var data = req.files.inputFile.data
+  //var fd = fs.openSync(filename, 'w')
+  //fs.writeSync(fd, data)
+  //fs.closeSync(fd)
+  //var process = spawn('/usr/bin/python3', [filename])
+  //var result = process.stdout
+  //console.log(result)
+  //res.end(result)
+  //// res.send('test')
+//})
 
 app.listen(port, err => {
   if (err) {
